2c028014d601e711d92635547eb3f7094ca60880
kuangc <kcckenny@163.com>
2019-12-02 18:18:57 +0800

    first

---
+++ src/main/java/top/kuangc/hellomybatis/HellomybatisApplication.java
@@ -0,0 +1,13 @@
+package top.kuangc.hellomybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellomybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellomybatisApplication.class, args);
+    }
+
+}
